5dd2a3455725d8acda3f8f571639020ea2f41513
animetosho <animetosho@users.noreply.github.com>
2019-11-16 05:01:07 +1000

    Dedupe EOL handling with main loop in SSE encoder

--- src/encoder_sse_base.h
+++ src/encoder_sse_base.h
@@ -184,182 +184,28 @@ static HEDLEY_ALWAYS_INLINE __m128i sse2_expand_bytes(int mask, __m128i data) {
 
 
 template<enum YEncDecIsaLevel use_isa>
-static HEDLEY_ALWAYS_INLINE void encode_eol_handle_pre(const uint8_t* HEDLEY_RESTRICT es, long& i, uint8_t*& p, long& col, long lineSizeOffset) {
-	uint32_t eolChar = lookups.eolLastChar[es[i]];
-	*(uint32_t*)p = eolChar;
-	p += 3 + (eolChar>>27);
-	
-	__m128i data = _mm_loadu_si128((__m128i *)(es + i + 1));
-	__m128i cmp;
-	unsigned int mask;
-	
-	// search for special chars
-#if defined(__SSSE3__) && !defined(__tune_atom__) && !defined(__tune_silvermont__) && !defined(__tune_btver1__)
-	if(use_isa >= ISA_LEVEL_SSSE3) {
-		cmp = _mm_cmpeq_epi8(
-			// TODO: optimize table
-			_mm_shuffle_epi8(_mm_set_epi8(
-				'='-42,'\0'-42,-128,-128,'\r'-42,' '-42,'\0'-42,'\n'-42,'\0'-42,'\r'-42,'.'-42,'\r'-42,'\n'-42,'\t'-42,'\n'-42,'\t'-42
-			), _mm_adds_epi8(
-				_mm_adds_epu8(data, _mm_set_epi8(
-					0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,22
-				)),
-				_mm_set_epi8(
-					120,120,120,120,120,120,120,120,120,120,120,120,120,120,120,91
-				)
-			)),
-			data
-		);
-		mask = _mm_movemask_epi8(cmp);
-	} else
-#endif
-	{
-		cmp = _mm_or_si128(
-			_mm_or_si128(
-				_mm_cmpeq_epi8(data, _mm_set1_epi8(-42)),
-				_mm_cmpeq_epi8(data, _mm_set1_epi8('\n'-42))
-			),
-			_mm_or_si128(
-				_mm_cmpeq_epi8(data, _mm_set1_epi8('='-42)),
-				_mm_cmpeq_epi8(data, _mm_set1_epi8('\r'-42))
-			)
-		);
-		mask = _mm_movemask_epi8(cmp);
-		mask |= lookups.eolFirstMask[es[i+1]];
-	}
-	i += XMM_SIZE + 1;
-	
-	if (LIKELIHOOD(0.2227, mask != 0)) {
-		uint8_t m1 = mask & 0xFF;
-		uint8_t m2 = mask >> 8;
-		__m128i shufMA, shufMB; // only used for SSSE3 path
-		__m128i data2;
-		
-#if defined(__AVX512VBMI2__) && defined(__AVX512VL__) && defined(__AVX512BW__) && defined(__POPCNT__)
-		if(use_isa >= ISA_LEVEL_VBMI2) {
-			data = _mm_sub_epi8(data, _mm_ternarylogic_epi32(cmp, _mm_set1_epi8(-42), _mm_set1_epi8(-42-64), 0xac));
-			
-			data2 = _mm_mask_expand_epi8(_mm_set1_epi8('='), lookups.expandMask[m2], _mm_srli_si128(data, 8));
-			data = _mm_mask_expand_epi8(_mm_set1_epi8('='), lookups.expandMask[m1], data);
-		} else
-#endif
-#ifdef __SSSE3__
-		if(use_isa >= ISA_LEVEL_SSSE3) {
-			// perform lookup for shuffle mask
-			shufMA = _mm_load_si128(&(lookups.shufMix[m1].shuf));
-			shufMB = _mm_load_si128(&(lookups.shufMix[m2].shuf));
-			
-			// second mask processes on second half, so add to the offsets
-			shufMB = _mm_or_si128(shufMB, _mm_set1_epi8(120));
-			
-			// expand halves
-			data2 = _mm_shuffle_epi8(data, shufMB);
-			data = _mm_shuffle_epi8(data, shufMA);
-			
-			// add in escaped chars
-			__m128i shufMixMA = _mm_load_si128(&(lookups.shufMix[m1].mix));
-			__m128i shufMixMB = _mm_load_si128(&(lookups.shufMix[m2].mix));
-			data = _mm_add_epi8(data, shufMixMA);
-			data2 = _mm_add_epi8(data2, shufMixMB);
-		} else
-#endif
-		{
-			if(HEDLEY_UNLIKELY(mask & (mask-1))) {
-				data2 = sse2_expand_bytes(m2, _mm_srli_si128(data, 8));
-				data = sse2_expand_bytes(m1, data);
-				// add in escaped chars
-				__m128i shufMixMA = _mm_load_si128(&(lookups.shufMix[m1].mix));
-				__m128i shufMixMB = _mm_load_si128(&(lookups.shufMix[m2].mix));
-				data = _mm_add_epi8(data, shufMixMA);
-				data2 = _mm_add_epi8(data2, shufMixMB);
-			} else {
-				// shortcut for common case of only 1 bit set
-#if defined(__LZCNT__) && defined(__tune_amdfam10__)
-				// lzcnt is faster than bsf on AMD
-				long bitIndex = _lzcnt_u32(mask);
-				__m128i mergeMask = _mm_load_si128((const __m128i*)lookups.expand_maskmix_lzc + bitIndex*2);
-				__m128i mixVals = _mm_load_si128((const __m128i*)lookups.expand_maskmix_lzc + bitIndex*2 + 1);
-#else
-				long _BSR_VAR(bitIndex, mask);
-				__m128i mergeMask = _mm_load_si128((const __m128i*)lookups.expand_maskmix_bsr + bitIndex*2);
-				__m128i mixVals = _mm_load_si128((const __m128i*)lookups.expand_maskmix_bsr + bitIndex*2 + 1);
-#endif
-				data = _mm_or_si128(
-					_mm_and_si128(mergeMask, data),
-					_mm_slli_si128(_mm_andnot_si128(mergeMask, data), 1)
-				);
-				// add escape chars
-				data = _mm_add_epi8(data, mixVals);
-				
-				// store main part
-				STOREU_XMM(p, data);
-				// store final char
-				p[XMM_SIZE] = es[i-1] + 42 + (64 & (mask>>(XMM_SIZE-1-6)));
-				
-				p += XMM_SIZE + 1;
-				col = lineSizeOffset+1;
-				
-				return;
-			}
-		}
-		
-		// store out
-		unsigned int shufALen, shufTotalLen;
-#if defined(__POPCNT__) && (defined(__tune_znver2__) || defined(__tune_znver1__) || defined(__tune_btver2__))
-		if(use_isa >= ISA_LEVEL_AVX) {
-			shufALen = popcnt32(m1) + 8;
-			shufTotalLen = popcnt32(mask) + 16;
-		} else
-#endif
-		{
-			shufALen = lookups.BitsSetTable256plus8[m1];
-			shufTotalLen = shufALen + lookups.BitsSetTable256plus8[m2];
-		}
-		STOREU_XMM(p, data);
-		STOREU_XMM(p+shufALen, data2);
-		p += shufTotalLen;
-		col = (long)(shufTotalLen) + lineSizeOffset-16;
-	} else {
-		data = _mm_sub_epi8(data, _mm_set1_epi8(-42));
-		STOREU_XMM(p, data);
-		p += XMM_SIZE;
-		col = lineSizeOffset;
-	}
-	// TODO: check col >= 0 if we want to support short lines
-}
-
-
-template<enum YEncDecIsaLevel use_isa>
 HEDLEY_ALWAYS_INLINE void do_encode_sse(int line_size, int* colOffset, const uint8_t* HEDLEY_RESTRICT srcEnd, uint8_t* HEDLEY_RESTRICT& dest, size_t& len) {
 	if(len < XMM_SIZE*2+1 || line_size < XMM_SIZE*2+1) return;
 	
 	uint8_t *p = dest; // destination pointer
 	long i = -(long)len; // input position
-	long lineSizeOffset = -line_size +16 +1; // line size plus vector length
+	long lineSizeOffset = -line_size +1;
 	//long col = *colOffset - line_size +1; // for some reason, this causes GCC-8 to spill an extra register, causing the main loop to run ~5% slower, so use the alternative version below
-	long col = *colOffset + lineSizeOffset - 16;
+	long col = *colOffset + lineSizeOffset;
 	
 	// offset position to enable simpler loop condition checking
 	const int INPUT_OFFSET = XMM_SIZE + XMM_SIZE+1 -1; // EOL handling performs a full 128b read, -1 to change <= to <
 	i += INPUT_OFFSET;
 	const uint8_t* es = srcEnd - INPUT_OFFSET;
 	
-	if (LIKELIHOOD(0.999, col == -line_size+1)) {
-		uint8_t c = es[i++];
-		if (LIKELIHOOD(0.0273, escapedLUT[c] != 0)) {
-			*(uint16_t*)p = escapedLUT[c];
-			p += 2;
-			col += 2;
-		} else {
-			*(p++) = c + 42;
-			col += 1;
-		}
-	}
 	if(LIKELIHOOD(0.001, col >= 0)) {
-		if(col == 0)
-			encode_eol_handle_pre<use_isa>(es, i, p, col, lineSizeOffset);
-		else {
-			uint8_t c = es[i++];
+		uint8_t c = es[i++];
+		if(col == 0) {
+			uint32_t eolChar = lookups.eolLastChar[c];
+			*(uint32_t*)p = eolChar;
+			p += 3 + (eolChar>>27);
+			col = -line_size+1;
+		} else {
 			if (LIKELIHOOD(0.0273, escapedLUT[c]!=0)) {
 				*(uint32_t*)p = UINT32_16_PACK(UINT16_PACK('\r', '\n'), (uint32_t)escapedLUT[c]);
 				p += 4;
@@ -371,7 +217,18 @@ HEDLEY_ALWAYS_INLINE void do_encode_sse(int line_size, int* colOffset, const uin
 			}
 		}
 	}
-	while(i < 0) {
+	if (LIKELIHOOD(0.999, col == -line_size+1)) {
+		uint8_t c = es[i++];
+		if (LIKELIHOOD(0.0273, escapedLUT[c] != 0)) {
+			*(uint16_t*)p = escapedLUT[c];
+			p += 2;
+			col += 2;
+		} else {
+			*(p++) = c + 42;
+			col += 1;
+		}
+	}
+	do {
 		__m128i data = _mm_loadu_si128((__m128i *)(es + i)); // probably not worth the effort to align
 		i += XMM_SIZE;
 		// search for special chars
@@ -400,6 +257,8 @@ HEDLEY_ALWAYS_INLINE void do_encode_sse(int line_size, int* colOffset, const uin
 		}
 		
 		unsigned int mask = _mm_movemask_epi8(cmp);
+		
+		_encode_loop_branch:
 		// likelihood of non-0 = 1-(63/64)^(sizeof(__m128i))
 		if (LIKELIHOOD(0.2227, mask != 0)) { // seems to always be faster than _mm_test_all_zeros, possibly because http://stackoverflow.com/questions/34155897/simd-sse-how-to-check-that-all-vector-elements-are-non-zero#comment-62475316
 			uint8_t m1 = mask & 0xFF;
@@ -584,10 +443,50 @@ HEDLEY_ALWAYS_INLINE void do_encode_sse(int line_size, int* colOffset, const uin
 				i -= col;
 				
 				_encode_eol_handle_pre:
-				encode_eol_handle_pre<use_isa>(es, i, p, col, lineSizeOffset);
+				uint32_t eolChar = lookups.eolLastChar[es[i]];
+				*(uint32_t*)p = eolChar;
+				p += 3 + (eolChar>>27);
+				col = lineSizeOffset;
+				
+				data = _mm_loadu_si128((__m128i *)(es + i + 1));
+				// search for special chars (EOL)
+#if defined(__SSSE3__) && !defined(__tune_atom__) && !defined(__tune_silvermont__) && !defined(__tune_btver1__)
+				if(use_isa >= ISA_LEVEL_SSSE3) {
+					cmp = _mm_cmpeq_epi8(
+						_mm_shuffle_epi8(_mm_set_epi8(
+							'='-42,'\0'-42,-128,-128,'\r'-42,' '-42,'\0'-42,'\n'-42,'\0'-42,'\r'-42,'.'-42,'\r'-42,'\n'-42,'\t'-42,'\n'-42,'\t'-42
+						), _mm_adds_epi8(
+							_mm_adds_epu8(data, _mm_set_epi8(
+								0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,22
+							)),
+							_mm_set_epi8(
+								120,120,120,120,120,120,120,120,120,120,120,120,120,120,120,91
+							)
+						)),
+						data
+					);
+					mask = _mm_movemask_epi8(cmp);
+				} else
+#endif
+				{
+					cmp = _mm_or_si128(
+						_mm_or_si128(
+							_mm_cmpeq_epi8(data, _mm_set1_epi8(-42)),
+							_mm_cmpeq_epi8(data, _mm_set1_epi8('\n'-42))
+						),
+						_mm_or_si128(
+							_mm_cmpeq_epi8(data, _mm_set1_epi8('='-42)),
+							_mm_cmpeq_epi8(data, _mm_set1_epi8('\r'-42))
+						)
+					);
+					mask = _mm_movemask_epi8(cmp);
+					mask |= lookups.eolFirstMask[es[i+1]];
+				}
+				i += XMM_SIZE + 1;
+				goto _encode_loop_branch;
 			}
 		}
-	}
+	} while(i < 0);
 	
 	*colOffset = col + line_size -1;
 	dest = p;
