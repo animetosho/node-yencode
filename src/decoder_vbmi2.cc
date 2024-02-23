4248373cd68be24db6dd7127f0b1c16740ba4275
animetosho <animetosho@users.noreply.github.com>
2024-02-23 19:25:58 +1000

    VBMI2 decoder doesn't need any lookup tables

--- src/decoder_vbmi2.cc
+++ src/decoder_vbmi2.cc
@@ -12,9 +12,6 @@ const bool decoder_has_avx10 = false;
 # ifndef YENC_DISABLE_AVX256
 #  include "decoder_avx2_base.h"
 void decoder_set_vbmi2_funcs() {
-	ALIGN_ALLOC(lookups, sizeof(*lookups), 16);
-	// TODO: consider removing compact LUT
-	decoder_init_lut(lookups->compact);
 	_do_decode = &do_decode_simd<false, false, sizeof(__m256i)*2, do_decode_avx2<false, false, ISA_LEVEL_VBMI2> >;
 	_do_decode_raw = &do_decode_simd<true, false, sizeof(__m256i)*2, do_decode_avx2<true, false, ISA_LEVEL_VBMI2> >;
 	_do_decode_end_raw = &do_decode_simd<true, true, sizeof(__m256i)*2, do_decode_avx2<true, true, ISA_LEVEL_VBMI2> >;
@@ -23,8 +20,6 @@ void decoder_set_vbmi2_funcs() {
 # else
 #  include "decoder_sse_base.h"
 void decoder_set_vbmi2_funcs() {
-	decoder_sse_init();
-	decoder_init_lut(lookups->compact);
 	_do_decode = &do_decode_simd<false, false, sizeof(__m128i)*2, do_decode_sse<false, false, ISA_LEVEL_VBMI2> >;
 	_do_decode_raw = &do_decode_simd<true, false, sizeof(__m128i)*2, do_decode_sse<true, false, ISA_LEVEL_VBMI2> >;
 	_do_decode_end_raw = &do_decode_simd<true, true, sizeof(__m128i)*2, do_decode_sse<true, true, ISA_LEVEL_VBMI2> >;
