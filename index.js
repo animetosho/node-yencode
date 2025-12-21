392a5ac1ff145a0c92753b571ad0039e804546f3
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2025-12-21 22:21:16 +0100

    fix: prebuild support

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var y = require('./build/Release/yencode.node');
+var y = require('node-gyp-build')(__dirname);
 
 var toBuffer = Buffer.alloc ? Buffer.from : Buffer;
 var allocBuffer = (Buffer.allocUnsafe || Buffer);
